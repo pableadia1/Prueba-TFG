53a2f2123f042021a67af8aab6f07a1395673304
pableadia1 <pablosevillista_98@hotmail.com>
2021-10-14 16:26:59 +0200

    test

--- FutbolStats/tests.py
+++ FutbolStats/tests.py
@@ -1,3 +1,14 @@
+from unittest import result
 from django.test import TestCase
+from .views import *
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def test_zeros(self):
+        data = "70"
+        resultado_esperado = 70  
+        self.assertEqual(Zeros(data), resultado_esperado)
+    
+    def test_demarcaciones(self):
+        data = "(MCO-DEL-EI)"
+        resultado_esperado = ["MCO","DEL","EI"]
+        self.assertEqual(demarcaciones(data), resultado_esperado)
