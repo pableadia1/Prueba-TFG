f87fb4b0ce6ffe327d51134cc051502f0b1eecdc
pableadia1 <pablosevillista_98@hotmail.com>
2021-10-17 18:40:58 +0200

    13

--- FutbolStats/tests.py
+++ FutbolStats/tests.py
@@ -12,3 +12,17 @@ class SimpleTest(TestCase):
         data = "(MCO-DEL-EI)"
         resultado_esperado = ["MCO","DEL","EI"]
         self.assertEqual(demarcaciones(data), resultado_esperado)
+
+class CreacionTest(TestCase):
+    def setUp(self):
+        self.jugador = Jugador.objects.create(nombre="jugador1",nombreCompleto="jugado1",nacionalidad="España",pie="Izquierda",posicion="PO",foto="foto.jpg") 
+        esJug = EstadisticasJugador.objects.create(jugador=self.jugador,temporada="2021-2020",equpo="Sevilla")
+        lisJug = [esJug]
+        lisEstCr = [(5,7)]
+        estadisticasCreacion(lisEstCr,lisJug)
+    
+    def test_creacion(self):
+        self.assertEqual(EstadisticasCreacion.pasesGol,5)
+    
+    def tearDown(self):
+        self.jugador.delete()
